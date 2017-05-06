3c8076a6f623440484b3f5bf19922c0c965d33c5
Muhammad Eakub <meakub86@gmail.com>
2017-05-06 00:26:08 -0400

    updated UI and component classes

--- CrudApp/src/app/app.component.ts
+++ CrudApp/src/app/app.component.ts
@@ -2,6 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: './app.component.html',
+  styles:[`
+          body{ text-align:center; width:100%; } 
+          .myContainer{ width:100%; height:200px;} 
+          .operaiton{ 
+              background: grey; width: 200px; 
+              padding: 50px; float: left; 
+              text-align: center; 
+              border:solid 5px darkgreen; 
+              margin: 3px; font-size:20px; font-weight:bold; }
+          .operaiton:hover {
+              background-color: #EEE;
+              cursor: pointer;
+              color: #607d8b;
+              }
+           `]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { title = 'Welocome to AngularJS 4 CRUD operation App !!!'; }
