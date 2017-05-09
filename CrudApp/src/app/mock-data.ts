0bd40901dad7c5e26379a02f130ac6133adada14
Muhammad Eakub <meakub86@gmail.com>
2017-05-08 21:49:16 -0400

    added mock data source

---
+++ CrudApp/src/app/mock-data.ts
@@ -0,0 +1,10 @@
+import { Employee } from './employee';
+
+export const employees: Employee[] = [
+    new Employee( 'Ehan', 'CTO', 1, 100000),
+    new Employee('Yusuf', 'Director', 1, 100000),
+    new Employee('Muhammad', 'Developer', 1, 90000),
+    new Employee('Ehan', 'CTO', 1, 100000),
+    new Employee('Karim', 'GM', 1, 100000),
+    new Employee('Rahim', 'CFO', 1, 100000),
+];
